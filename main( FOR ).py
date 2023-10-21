c6691f34c392fa28e6ce5a33b3f8f644aebb6e69
Akshay <109850853+Prince-ak007@users.noreply.github.com>
2023-10-22 04:28:37 +0530

    Add files via upload

---
+++ main( FOR ).py
@@ -0,0 +1,60 @@
+# logical operators (and , or)
+#AND ==>> both conditions should be True.
+#OR ==>> either of condition should be True. 
+
+age = 19
+voter_id = True 
+if age > 18 and  voter_id:
+  print("yes")
+else:
+  print("cant")
+
+
+# for loop :-
+
+for i in 1,2,3,4,5:
+  print(i)
+print()
+print("--------------------------------------")
+print()
+
+number =   1 ,5 , 8 , 7 
+sara = "hello akshay" # index like h e l l o ....
+num  = ["happy", "belt", "come", "snoopdog", "elephant"]  # print as a list (on by on)
+
+
+for i in number:
+  print(i)
+
+print()
+print("--------------------------------------")
+print()
+
+
+#for in dict 
+
+dict = {
+  "name":"akshay",
+  "job": "soon",
+  "life":"so so"  
+}
+
+for key,value in dict.items():
+  print(key)
+  print(value)
+
+#or
+
+for key in dict.keys():
+  print(key)
+
+print()
+print("--------------------------------------")
+print()
+
+for mun in range(1,5):
+  print("hello world")
+ 
+
+
+  
