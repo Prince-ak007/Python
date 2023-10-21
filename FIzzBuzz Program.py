a8466980468a46f3fddd9dd6f8d0d7f93aeb3dbb
Akshay <109850853+Prince-ak007@users.noreply.github.com>
2023-10-22 04:54:46 +0530

    FIzzBuzz Program.py

--- FIzzBuzz Program.py
+++ FIzzBuzz Program.py
@@ -3,16 +3,16 @@
 multiple of 5 ---> Buzz
 multiple of 3 and 5 ---> FizzBuzz"""""
 
-#solution 1) without adding into the list
-# for i in range(1,25):
-#   if i % 3 == 0 and  i % 5 == 0:
-#     print("FizzBuzz")
-#   elif i % 3 == 0:
-#     print("Fizz")
-#   elif i % 5 == 0:
-#     print("buzz")
-#   else:
-#     print(i)
+solution 1) without adding into the list
+for i in range(1,25):
+  if i % 3 == 0 and  i % 5 == 0:
+    print("FizzBuzz")
+  elif i % 3 == 0:
+    print("Fizz")
+  elif i % 5 == 0:
+    print("buzz")
+  else:
+    print(i)
 
 
 #solution 2) adding into the list
