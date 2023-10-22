8353352d2063faf187dd7601303154c5251b7ff1
Akshay <109850853+Prince-ak007@users.noreply.github.com>
2023-10-22 10:48:59 +0530

    Add files via upload

---
+++ calcy.py
@@ -0,0 +1,12 @@
+def add(num1,num2):
+  return num1 + num2 
+
+def sub(num1,num2):
+  return num1 - num2
+
+def even_odd(num):
+  if num % 2 == 0:
+    print("even")
+  else:
+    print("odd")
+
---
+++ main -- calcy.py
@@ -0,0 +1,6 @@
+import calcy
+
+result = calcy.add(10,20)
+print(result)
+
+calcy.even_odd(50)
