43e5e00762394bea5186b9a207227235de1b0e31
Akshay <109850853+Prince-ak007@users.noreply.github.com>
2023-10-22 06:21:41 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,80 @@
+#while Loop 
+# basic program of while 
+# i = 0
+# while i < 10:
+#   print(i)
+#   i+=1
+
+
+print()
+print("-----------------------")
+print()
+# making program which will run for 10 times  
+
+# i = 0
+# while i < 10:
+#  num = int(input(f"enter your number {i} :  "))
+#  if num % 5 ==0:
+#    print("yes sir")
+#  else:
+#    print("no sir ")
+#  i = i + 1
+ 
+# print("end of the program")
+
+#program with quit option 
+
+# user = ""
+# while user != "q":
+#   user = input("enter the num. or q for quit  ")
+#   if user.isdigit():
+#     if int(user) % 5 == 0:
+#       print("divisible ")
+#     else:
+#       print("not divisible")
+
+# print("bye bye ")
+
+
+
+# more exmples of while lOoP
+
+# count = 1
+# while count <=10:
+#   count = int(input("enter the number only   "))
+#   print(count)
+#   count+=1
+
+
+# msg = ""
+# while msg != "quit":
+#   msg = input("enter your message  ")
+#   print(msg)
+
+
+# using switch in wile loop 
+
+# active  =  True
+# while active:
+#   user = input("enter the message :   ")
+#   if user == "exit":
+#     active = False 
+#   else:
+#     print(user)
+
+
+#find and remove 
+
+list = [2,29,54,54,87,99,65,65,65]
+while 29 in list:
+  list.remove(29)
+print(list)
+
+
+
+
+
+
+
+ 
+
