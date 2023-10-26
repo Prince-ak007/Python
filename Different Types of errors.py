2104a7e372b50fa25930373936ef5cf13da1c75a
Akshay Kuthe <109850853+Prince-ak007@users.noreply.github.com>
2023-10-26 06:18:53 +0530

    Add files via upload

---
+++ Different Types of errors.py
@@ -0,0 +1,20 @@
+# EXception (Error) Handling
+'''
+DIFFERENT TYPES OF ERRORS
+ZeroDivisionError ==> e.g. print(a/0)
+NameError ==> e.g. var is not defined earlier
+TypeError ==> concatenating  error  print("10" + 10)
+ValueError ==> 
+IndexError ==> e.g list = [1,2,3,4] 
+print(list[5])
+
+KeyError ==> dict = {"name" : "Akshay" , 
+                    age: 27}
+                    print(dict["naame"])
+
+ModuleNotFoundError
+ImportError 
+
+
+
+'''
